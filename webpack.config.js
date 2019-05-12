cb8fd2a1bcbd1e4276ed07acea754c05fb9a11ff
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-05-12 13:26:15 +0900

    Use webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const rootPath = path.resolve('');
+
+const config = {
+  context: rootPath,
+  entry: './src/main.jsx',
+  output: {
+    path: `${rootPath}/docs`,
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: [/node_modules/],
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: `${rootPath}/docs`,
+    watchContentBase: true,
+    host: '0.0.0.0',
+    port: 1234,
+    inline: false,
+  },
+};
+
+module.exports = config;
