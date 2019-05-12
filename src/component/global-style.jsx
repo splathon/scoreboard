6ac02c68d108e6c576a42dd5c5244f94cd4492b7
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-05-13 00:36:27 +0900

    Add favicon and font

--- src/component/global-style.jsx
+++ src/component/global-style.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
 
-const GlobalStyle = () => (
-  <Global styles={css({
-    body: {
-      margin: 0,
-      backgroundColor: '#333',
-      color: '#fff'
-    }
-  })} />
-);
+const fontStyle = css`
+  @font-face {
+      font-family: Splatoon2;
+      src: url(./font/splatoon2-en.woff2) format("woff2");
+  }
+  @font-face {
+      font-family: Splatoon2;
+      src: url(./font/splatoon2-ja.woff2) format("woff2");
+  }
+`;
+
+const bodyStyle = css({
+  body: {
+    margin: 0,
+    backgroundColor: '#333',
+    color: '#fff',
+    fontFamily: 'Splatoon2, BlinkMacSystemFont, -apple-system, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
+  }
+});
+
+const GlobalStyle = () => <Global styles={[fontStyle, bodyStyle]} />;
 
 export default GlobalStyle;
