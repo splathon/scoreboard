a1514b237dbd1d8593a394961c67390cbe13e3a9
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-05-17 09:05:51 +0900

    Fix csv parser

--- src/extreme-union-match-1/csv-parser.js
+++ src/extreme-union-match-1/csv-parser.js
@@ -1,21 +1,33 @@
 export default ({ base, team }) => {
   const baseCells = base;
-  const teamCells= team;
+  const teamCells = team;
 
   console.log(base);
   console.log(team);
 
-  const [,, alfaName, alfaLife, ,,, minIdx] = baseCells[1];
-  const [,, bravoName, bravoLife, ,,, maxIdx] = baseCells[2];
+  // parse match teams
+  const [,, alfaName, alfaLife, alfaLifeMax, ,,, minIdx] = baseCells[1];
+  const [,, bravoName, bravoLife, bravoLifeMax, ,,, maxIdx] = baseCells[2];
 
-  const alfa = { team: alfaName, members: [], life: alfaLife|0 };
-  const bravo = { team: bravoName, members: [], life: bravoLife|0 };
+  const alfa = {
+    name: alfaName,
+    members: [],
+    life: alfaLife|0,
+    lifeMax: alfaLifeMax|0,
+  };
+  const bravo = {
+    name: bravoName,
+    members: [],
+    life: bravoLife|0,
+    lifeMax: bravoLifeMax|0,
+  };
 
+  // parse match results
   const results = [];
   const resultsRange = { min: minIdx|0, max: maxIdx|0 };
   const resultCells = baseCells.slice(
-    6 + resultsRange.min,
-    6 + resultsRange.max + 1
+    5 + resultsRange.min,
+    5 + resultsRange.max + 1
   );
   for (const result of resultCells) {
     const [, rule, stage, winner, ko] = result;
@@ -24,6 +36,7 @@ export default ({ base, team }) => {
     });
   }
 
+  // fill team members
   for (const team of teamCells) {
     if (team[0] === alfa.team) {
       alfa.members = team.slice(1, 5);
@@ -33,11 +46,24 @@ export default ({ base, team }) => {
     }
   }
 
+  // fill defaults
+  if (alfa.name.length === 0) {
+    alfa.name = 'アルファチーム';
+  }
+  if (bravo.name.length === 0) {
+    bravo.name = 'ブラボーチーム';
+  }
+  if (alfa.members.length === 0) {
+    alfa.members = ['イカ1', 'イカ2', 'イカ3', 'イカ4'];
+  }
+  if (bravo.members.length === 0) {
+    bravo.members = ['タコ1', 'タコ2', 'タコ3', 'タコ4'];
+  }
+
   console.log({ alfa, bravo });
   console.log(results);
 
   return {
-    totalWinCount: 3,
     matching: { alfa, bravo },
     results
   };
